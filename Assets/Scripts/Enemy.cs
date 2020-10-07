fc598c79787503b3d241d25d8539862088a822c4
EgemenArdaYurdakul <earda@sabanciuniv.edu>
2020-10-07 22:58:25 +0300

    Add terrain detail

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,11 +9,13 @@ public class Enemy : MonoBehaviour
     [SerializeField] private int points;
     [SerializeField] private int health;
     ScoreBehavior scoreBehavior;
+    private bool isDead;
 
     // Start is called before the first frame update
     void Start()
     {
         scoreBehavior = FindObjectOfType<ScoreBehavior>();
+        isDead = false;
     }
 
     // Update is called once per frame
@@ -25,8 +27,9 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         health--;
-        if (health < 0)
+        if (health < 0 && !isDead)
         {
+            isDead = true;
             GameObject explosion = Instantiate(explosionFX, transform.position, Quaternion.identity);
             explosion.transform.parent = parent;
             Destroy(this.gameObject);
